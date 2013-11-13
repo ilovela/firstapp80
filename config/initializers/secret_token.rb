ac5669a3e50f347c800bef2617646beb7baa5d22
ilovela <zhengchanapp@hotmail.com>
2013-11-12 18:06:49 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firstapp80::Application.config.secret_token = '521ad02690e4cd0b7357a550988035614e625e6a28e6338786b1c50f2ada0239c0bce3e24f46d5ba7ab4f7c0f97ae3c8e4358ce2f5f7c7c11623d23206ddb861'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Firstapp80::Application.config.session_store :cookie_store, key: '_firstapp80_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Firstapp80::Application.config.session_store :active_record_store
